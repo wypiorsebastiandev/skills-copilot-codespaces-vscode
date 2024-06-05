104c93f10a6b32c82fb190215f6fe33eb2ce9388
wypiorsebastiandev <168440431+wypiorsebastiandev@users.noreply.github.com>
2024-06-05 06:28:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,72 +1,26 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var qs = require('querystring');
-var comments = [];
-//create server
-http.createServer(function(req,res){
-  //parse the url
-  var urlObj = url.parse(req.url,true);
-  //get path
-  var pathName = urlObj.pathname;
-  //get method
-  var method = req.method;
-  //get query
-  var query = urlObj.query;
-  //get post data
-  var str = '';
-  req.on('data',function(data){
-    str += data;
-  });
-  req.on('end',function(){
-    var post = qs.parse(str);
-    //read file
-    fs.readFile('./index.html',function(err,data){
-      if(err){
-        console.log(err);
-      }else{
-        //deal with the path
-        if(pathName == '/'){
-          //get comments
-          fs.readFile('./comments.json','utf-8',function(err,commentsData){
-            if(err){
-              console.log(err);
-            }else{
-              comments = JSON.parse(commentsData);
-              //replace the comments
-              var htmlStr = data.toString();
-              var lis = '';
-              comments.forEach(function(comment){
-                lis += '<li>'+comment+'</li>';
-              });
-              htmlStr = htmlStr.replace('^_^',lis);
-              res.end(htmlStr);
-            }
-          });
-        }else if(pathName == '/comment'){
-          //add comment
-          comments.push(post.comment);
-          //write comments to the file
-          fs.writeFile('./comments.json',JSON.stringify(comments),function(err){
-            if(err){
-              console.log(err);
-            }else{
-              //redirect
-              res.statusCode = 302;
-              res.setHeader('Location','/');
-              res.end();
-            }
-          });
-        }else{
-          //404
-          res.statusCode = 404;
-          res.end('Not Found');
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+//set up the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+//set up the route
+app.get('/', (req, res) => {
+    res.send('Welcome to my web server!');
+});
+
+//set up the route
+app.get('/comments', (req, res) => {
+    fs.readFile('./comments.json', 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).send('Error reading file');
+            return;
         }
-      }
+        res.send(data);
     });
-  });
-}).listen(3000,function(){
-  console.log('Server is running at port 3000');
 });
