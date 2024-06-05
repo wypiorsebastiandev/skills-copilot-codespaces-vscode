d2690f830d6dd5d49b87c7fb59d9cb9ac324869d
wypiorsebastiandev <168440431+wypiorsebastiandev@users.noreply.github.com>
2024-06-05 06:14:28 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    // This is the "member" object
+    var member = {
+        // This is a "name" property
+        name: 'John Doe',
+        // This is a "skills" property
+        skills: ['JavaScript', 'HTML', 'CSS', 'React']
+    }
+    return member;
+}
